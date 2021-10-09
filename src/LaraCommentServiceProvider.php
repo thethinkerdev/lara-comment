0fa7f53729cbef315b031e7e8d8fb217cde8ddb3
samankhdev <samkh.dev@yahoo.com>
2021-10-09 06:50:16 +0330

    add migrations to publish

--- src/LaraCommentServiceProvider.php
+++ src/LaraCommentServiceProvider.php
@@ -10,7 +10,10 @@ class LaraCommentServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . "/database/migrations");
+        // $this->loadMigrationsFrom(__DIR__ . "/database/migrations");
+        $this->publishes([
+            __DIR__."/database/migrations/"=>database_path("/migrations")
+        ],'migrations');
     }
     
     public function register(){}
